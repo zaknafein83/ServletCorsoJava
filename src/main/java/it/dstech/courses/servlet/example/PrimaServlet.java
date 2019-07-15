8e219686a94c18d7b8e56855ad345858ede310b4
Utente <Utente@DESKTOP-SK19HRS.lan>
2019-07-15 10:47:49 +0200

    first commit

---
+++ src/main/java/it/dstech/courses/servlet/example/PrimaServlet.java
@@ -0,0 +1,20 @@
+package it.dstech.courses.servlet.example;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PrimaServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+		
+		System.out.println("test");
+	}
+
+}
