ea5aace80c110121e67ca603ec5a1ce03cda9cbb
Utente <Utente@DESKTOP-SK19HRS.lan>
2019-07-15 11:25:12 +0200

    prinma servlet funzionante

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ServletCorsoJava
+
+Per eseguire questa Servlet bisogna scaricare tomcat 8.5 da questo URL https://tomcat.apache.org/download-80.cgi
+
+Una volta avviato tomcat con la webappa deplyata, l'url da raggiungere è:
+
+http://localhost:8080/ServletCorsoJava/comingsoon
+
+
+Se si vuole vedere la welcome page, bisogna andare su
+http://localhost:8080/ServletCorsoJava
--- src/main/java/it/dstech/courses/servlet/example/PrimaServlet.java
+++ src/main/java/it/dstech/courses/servlet/example/PrimaServlet.java
@@ -2,6 +2,7 @@ package it.dstech.courses.servlet.example;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,10 +12,16 @@ public class PrimaServlet extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		super.doGet(req, resp);
-		
-		System.out.println("test");
+		System.out.println("doget");
+
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated masdfsaethod stub
+		super.doPost(req, resp);
+		System.out.println("dopst");
+		doGet(req, resp);
+	}
 }
